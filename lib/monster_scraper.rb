1f3ae4474e44945f5a336308ccf75e515573a7da
James Sutherland <jrsutherland78@googlemail.com>
2019-02-10 00:30:09 +0000

    add watir nokogiri gems to load page headless and extract names

--- lib/monster_scraper.rb
+++ lib/monster_scraper.rb
@@ -1,3 +1,32 @@
+require 'watir'
+require 'nokogiri'
+
 class MonsterScraper
+
+  attr_reader :monsters, :url
+
+  def initialize(url = "https://roll20.net/compendium/dnd5e/Monsters%20List#content")
+    @url = url
+    @monsters = []
+  end
+
+  def return_monsters
+    get_page_info.each do |el|
+      single_monster = {
+        name: el.css('a').text
+      }
+      @monsters << single_monster
+    end
+    @monsters
+  end
+
+  def get_page_info
+    unparsed_page = Watir::Browser.new(:chrome, headless: true)
+    unparsed_page.goto @url
+    Watir::Wait.until { unparsed_page.text.include? 'Aboleth'} # first monster
+    puts "DONE"
+    parse_page = Nokogiri::HTML.parse(unparsed_page.html)
+    parse_page.css('div.listResult.booktemplate.closed.list')
+  end
 
 end
--- spec/monster_scraper_spec.rb
+++ spec/monster_scraper_spec.rb
@@ -2,9 +2,19 @@ require 'monster_scraper'
 
 describe 'MonsterScraper' do
 
+  before(:all) do
+    mockurl = "file://#{Dir.pwd}/spec/mockHTML.html" # Dir.pwd is the local directory address on the project
+    @monster_scraper = MonsterScraper.new(mockurl)
+  end
+
   it 'contains MonsterScraper Class' do
-    monster_scraper = MonsterScraper.new
-    expect(monster_scraper).to be_a MonsterScraper
+    expect(@monster_scraper).to be_a MonsterScraper
+  end
+
+  describe '#return_monsters' do
+    it 'returns an array of hashes with monster names' do
+      expect(@monster_scraper.return_monsters).to eq([{ name: "Aboleth" }, { name: "Dragon" }, { name: "Zombie" }])
+    end
   end
 
 end
